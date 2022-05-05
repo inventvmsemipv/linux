313d61e2eff2b2f4ca62cf2bfca5a81a840e7af3
Davide Ciminaghi <ciminaghi@gnudd.com>
2022-05-05 09:55:27 +0200

    ASOC, codecs: add empty skeleton of ivm6303 codec driver
    Signed-off-by: Davide Ciminaghi <ciminaghi@gnudd.com>

---
+++ sound/soc/codecs/ivm6303.c
@@ -0,0 +1,150 @@
+/*
+ * ivm6303.c -- ivm6303 ALSA SoC Audio Driver
+ *
+ * Copyright 2022 Inventvm
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/regmap.h>
+#include <linux/mutex.h>
+#include <linux/of_gpio.h>
+#include <linux/workqueue.h>
+#include <linux/clk.h>
+#include <linux/input.h>
+#include <sound/soc.h>
+#include <sound/tlv.h>
+#include <sound/asound.h>
+#include <sound/soc-dai.h>
+#include <sound/pcm_params.h>
+#include <linux/pm_wakeup.h>
+#include <linux/kernel.h>
+
+/* Base Region */
+#define IVM6303_SYSTEM_CTRL		0x00
+
+#define IVM6303_PAGE_SELECTION		0xfe
+
+struct ivm6303_platform_data {
+};
+
+struct ivm6303_priv {
+	struct i2c_client	*i2c_client;
+	struct regmap		*regmap;
+};
+
+static const struct regmap_range_cfg ivm6303_range_cfg[] = {
+	{
+		.range_min = IVM6303_SYSTEM_CTRL,
+		/* From 0x100 to 0x1ff: undocumented test registers */
+		.range_max = 0x1ff,
+		.selector_reg = IVM6303_PAGE_SELECTION,
+		.selector_mask = 1,
+		.selector_shift = 0,
+		.window_start = 0,
+		.window_len = 256,
+	}
+};
+
+static bool ivm6303_readable_register(struct device *dev, unsigned int reg)
+{
+	return (reg <= 0x1ff);
+}
+
+static bool ivm6303_writeable_register(struct device *dev,
+				       unsigned int reg)
+{
+	return (reg <= 0x1ff);
+}
+
+static bool ivm6303_volatile_register(struct device *dev, unsigned int reg)
+{
+	return reg <= 0xff;
+}
+
+static const struct regmap_config regmap_config = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.ranges = ivm6303_range_cfg,
+	.num_ranges = ARRAY_SIZE(ivm6303_range_cfg),
+	.max_register = 0x1ff,
+	.readable_reg = ivm6303_readable_register,
+	.writeable_reg = ivm6303_writeable_register,
+	.volatile_reg = ivm6303_volatile_register,
+
+	.cache_type = REGCACHE_NONE,
+};
+
+static int ivm6303_probe(struct i2c_client *client)
+{
+	struct ivm6303_priv  *priv;
+	int ret = 0;
+
+	dev_notice(&client->dev, "%s: IVM6303 driver\n", __func__);
+
+	if (!i2c_check_functionality(client->adapter,
+				     I2C_FUNC_SMBUS_BYTE_DATA)) {
+		dev_err(&client->dev,
+			"%s: no support for i2c read/write byte data\n",
+			__func__);
+		return -EIO;
+	}
+	priv = devm_kzalloc(&client->dev, sizeof(struct ivm6303_priv),
+			    GFP_KERNEL);
+	if (priv == NULL) {
+		ret = -ENOMEM;
+		goto end;
+	}
+
+	priv->i2c_client = client;
+
+	priv->regmap = devm_regmap_init_i2c(priv->i2c_client, &regmap_config);
+	if (IS_ERR(priv->regmap)) {
+		ret = PTR_ERR(priv->regmap);
+		dev_err(&client->dev, "regmap init failed\n");
+	}
+
+	i2c_set_clientdata(client, priv);
+end:
+	return ret;
+}
+
+
+static void ivm6303_remove(struct i2c_client *client)
+{
+}
+
+static const struct of_device_id ivm6303_match_table[] = {
+	{ .compatible = "inventvm,ivm6303-amp", },
+	{}
+};
+
+static const struct i2c_device_id ivm6303_id[] = {
+	{ "ivm6303-amp", 0 },
+	{ },
+};
+
+MODULE_DEVICE_TABLE(i2c, ivm6303_id);
+
+static struct i2c_driver ivm6303_i2c_driver = {
+	.driver = {
+		.name		= "ivm6303-amp",
+		.owner		= THIS_MODULE,
+		.of_match_table = ivm6303_match_table,
+	},
+	.probe		= ivm6303_probe,
+	.remove		= ivm6303_remove,
+	.id_table	= ivm6303_id,
+};
+
+module_i2c_driver(ivm6303_i2c_driver);
+
+MODULE_DESCRIPTION("ASoC IVM6303 driver");
+MODULE_AUTHOR("Davide Ciminaghi, ciminaghi@gnudd.com");
+MODULE_LICENSE("GPL");
